f3f5656822e1cd76c62212b6cb0ad7934684e008
云影 <14521425+shadow-of-cloud@user.noreply.gitee.com>
2024-06-23 21:32:09 +0800

    blog

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -8,21 +8,30 @@ export default navbar([
     prefix: "/posts/",
     children: [
       {
-        text: "solution",
+        text: "Solution",
         icon: "shripm",
-        prefix: "solution/",
+        prefix: "Solution/",
         children: [
           {text: "Docker拉取国外镜像失败", icon: "shrimp", link: "Docker拉取国外镜像失败"},
         ]
       },
 
       {
-        text: "learning",
+        text: "Learning",
         icon: "book-open-reader",
-        prefix: "learning/",
+        prefix: "Learning/",
         children: [
           {text: "Linux基础知识", icon: "book-open-reader", link: "Linux基础知识"},
           {text: "Linux进阶-shell编程", icon: "book-open-reader", link: "Linux进阶-shell编程"},
+        ]
+      },
+
+      {
+        text: "tools",
+        icon: "user-secret",
+        prefix: "Tools/",
+        children: [
+          {text: "Shell Hmoe", icon: "bug", link: "Shell Hmoe"},
         ]
       },
     ],
---
+++ src/posts/Tools/Shell Home.md
@@ -0,0 +1,40 @@
+---
+icon: bug
+date: 2024-06-23
+category:
+  - Linux
+  - Shell
+tag:
+  - play
+---
+
+# Shell Home
+
+## 判断主机存活
+
+```shell
+#! /bin/bash
+
+num=0
+if [ $# -eq 0 ];then
+	echo -e "\e[1;31please enter an parameter after `basename $0` file\e[0m"
+	exit
+fi
+if [ ! -f $0 ];then
+	echo -e "\e[1;31mthe file type is not committed\e[0m"
+	exit
+fi
+for ip in `cat $1`
+do
+	ping -c1 $ip &>/etc/null
+	if [ $? ];then
+		echo -e "\e[1;34m$ip is up\e[0m"
+		((num++))
+	fi
+done
+if [ $num -eq 0 ];then
+	echo -e "\e[1;31mall die\e[0m"
+fi
+
+```
+

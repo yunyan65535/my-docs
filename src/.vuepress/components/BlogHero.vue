cb257ba57dab4d879c833646cae3adac45dcad8b
云影 <14521425+shadow-of-cloud@user.noreply.gitee.com>
2024-06-24 15:39:15 +0800

    blog

---
+++ src/.vuepress/components/BlogHero.vue
@@ -0,0 +1,16 @@
+<!-- .vuepress/components/BlogHero.vue -->
+<script setup lang="ts">
+import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
+import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
+import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+
+</script>
+
+<template>
+  <BlogHero>
+    <template #info="info" #bg>
+      <BingHeroBackground />
+      <HitokotoBlogHero v-bind="info" />
+    </template>
+  </BlogHero>
+</template>
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,9 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { commentPlugin } from '@vuepress/plugin-comment'
+import { getDirname, path } from "vuepress/utils";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -21,6 +24,13 @@ export default defineUserConfig({
       categoryId: "DIC_kwDOMLw8XM4CgNuN",
     }),
   ],
+
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
+  },
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,

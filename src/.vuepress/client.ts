19cb0d636e7c4709334459eb85c075c9c4be4d4c
云影 <14521425+shadow-of-cloud@user.noreply.gitee.com>
2025-04-27 20:48:01 +0800

    blog

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,22 @@
+import { defineClientConfig } from "vuepress/client";
+import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
+import { setupSnowFall } from "vuepress-theme-hope/presets/snowFall.js";
+import "vuepress-theme-hope/presets/round-blogger-avatar.scss";
+import "vuepress-theme-hope/presets/bounce-icon.scss"
+
+export default defineClientConfig({
+  setup() {
+    setupRunningTimeFooter(
+      new Date("2024-06-18"),
+      {
+        "/": "Running time: :day days :hour hours :minute minutes :second seconds",
+        "/zh/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
+      },
+      false,
+    );
+
+    setupSnowFall({
+      count: 20,
+    });
+  },
+});
